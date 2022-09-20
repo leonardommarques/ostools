cb7bc6bbba5310e50dfb098c6aef4125e89f388f
leonardo marques <leonardomarques@leonardos-MacBook-Pro.local>
2022-09-19 21:00:01 -0300

    create prediction functions

--- auxiliar.py
+++ auxiliar.py
@@ -1,3 +1,38 @@
+# # -------------------------------------------- #
+# data_by_labels = {}
+# ith_label = -1
+# while ith_label < len(np.unique(y) - 1):
+#     ith_label = ith_label + 1
+#     label = np.unique(y)[ith_label]
+#     # label = 2
+#     idx_label = y == label
+#     X_label = X[idx_label]
+#     y_label = y[idx_label]
+#
+#     X_other_labels = X[~idx_label]
+#     y_other_labels = y[~idx_label]
+#
+#     data_by_labels[label] = {}
+#     data_by_labels[label]['label'] = label
+#     data_by_labels[label]['X'] = X_label
+#     data_by_labels[label]['y'] = y_label
+#     del y_label, X_label, label
+#
+# # ----------------------------- #
+# # -- Get shortest distances
+# # ----------------------------- #
+# ith_label = -1
+# while ith_label < len(data_by_labels):
+#     ith_label = ith_label + 1
+#     str_label = list(data_by_labels.keys())[ith_label]
+#
+#     fetures = data_by_labels[str_label]['']
+#     fetures.keys()
+#
+#     ith_label
+# data_by_labels
+# data_by_labels
+
 
 import os
 os.path
@@ -79,3 +114,137 @@ get_metrics(
         , unknown_label=-1
         , average='macro'
         , metric='true_negative_rate')
+
+################################################################################
+################################################################################
+################################################################################
+import pandas as pd
+# sample_train = pd.read_csv('/Users/leonardomarques/Downloads/sample_train.csv')
+# sample_train = pd.read_csv('/Users/leonardomarques/Downloads/sample_train.csv')
+# X = sample_train[['x1', 'x2']].values
+# y = sample_train['group'].values
+
+
+train_da = pd.read_csv('/Users/leonardomarques/Downloads/train_da.csv')
+X_train = train_da[['x1', 'x2']].values
+y_train = train_da['group'].values
+
+val_da = pd.read_csv('/Users/leonardomarques/Downloads/val_da.csv')
+X_val = val_da[['x1', 'x2']].values
+y_val = val_da['group'].values
+
+test_da = pd.read_csv('/Users/leonardomarques/Downloads/test_da.csv')
+X_test = test_da[['x1', 'x2']].values
+y_test = test_da['group'].values
+
+train_da['group'].value_counts()
+
+evm = EVM(
+    margin_scale=1
+    , tail_size=20
+    , n_obs_to_fuse=4
+    , confidence_threshold=0.9
+)
+from datetime import datetime
+t0 = datetime.now()
+evm.fit(
+    X_train
+    , y_train
+)
+t1 = datetime.now()
+tempo_fit = (t1-t0).total_seconds()
+
+tempo_fit/60
+# -- check some weibull fits
+evm.weibulls_dict[1][0]
+
+probas_ = evm.predict_proba(X_train)
+predicted_labels = evm.predict(X_train)
+predicted_labels = predicted_labels.reshape(-1, 1)
+train_da['prediction'] = predicted_labels
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix(train_da['group'], train_da['prediction'], labels = np.sort(train_da['group'].unique()))
+train_da['group'].value_counts()
+
+sum(train_da['group'].value_counts())
+332513078856332513078856332513078856
+
+probas_ = evm.predict_proba(X_val)
+predicted_labels = evm.predict(X_val)
+predicted_labels = predicted_labels.reshape(-1, 1)
+val_da['prediction'] = predicted_labels
+confusion_matrix(val_da['group'], val_da['prediction'], labels = np.sort(val_da['group'].unique()))
+
+
+probas_ = evm.predict_proba(X_test)
+predicted_labels = evm.predict(X_test)
+predicted_labels = predicted_labels.reshape(-1, 1)
+test_da['prediction'] = predicted_labels
+
+
+
+test_da['prediction'].unique()
+test_da['prediction'].value_counts()
+
+train_da['prediction'].value_counts()
+val_da['prediction'].value_counts()
+
+self = evm
+return_dict = True
+from datetime import datetime
+
+t0 = datetime.now()
+probas_ = evm.predict_proba(X_train)
+t_pred = datetime.now()
+
+tempos_pred = [t0, t_pred]
+
+t0 = datetime.now()
+probas_ = evm.predict(X_train)
+t_pred2 = datetime.now()
+tempos_pred2 = [t0, t_pred2]
+
+# -------------------------------------------------- #
+# -------------------------------------------------- #
+# debug fit
+# -------------------------------------------------- #
+# -------------------------------------------------- #
+X = X_train
+y = y_train
+self = evm
+# evm.fit(
+#     X_train
+#     , y_train
+# )
+len(self.weibulls_dict)
+self.weibulls_dict[1]
+
+weibulls_dict[1]
+
+self.weibulls_dict[1]
+sum([len(i) for i in self.weibulls_dict.values()])
+self.weibulls_dict.keys()
+tempos = [
+    t0
+    , t_filtros
+    , t_tail
+    , t_fit
+]
+datetime.
+
+(tempos[1]-tempos[0]).total_seconds()
+(tempos[2]-tempos[1]).total_seconds()
+(tempos[3]-tempos[2]).total_seconds()
+
+
+t0 = datetime.now()
+stats.exponweib.fit(obs_distances, floc=0, f0=1)
+tfit2 = datetime.now()
+(tfit2-t0).total_seconds()
+np.array([obs_distances, obs_distances, obs_distances])
+
+t0 = datetime.now()
+stats.exponweib.fit(obs_distances, floc=0, f0=1, loc = 1, scale = 1)
+tfit2 = datetime.now()
+(tfit2-t0).total_seconds()
--- models/EVM.py
+++ models/EVM.py
@@ -7,60 +7,19 @@
 # from sklearn.metrics import precision_score
 
 import numpy as np
+import pandas as pd
 from sklearn.base import BaseEstimator, ClassifierMixin
 from scipy import stats
+from scipy.spatial import distance_matrix
 
-
-class EMV(BaseEstimator, ClassifierMixin):
-    """Fits a logistic regression model on tree embeddings.
-    """
-    def __init__(self
-                 , margin_scale = 1  # ?? For some reason this is used to scales the distances in distance matrix
-                 , tail_size = 20  # number of observations to use
-                 , **kwargs
-    ):
-        """
-        :param margin_scale: For some reason this is used to scales the distances in distance matrix.
-        :param tail_size: number of observations to use in the prediction by averaging out their individual predictions
-        :param kwargs:
-        """
-
-        self.kwargs = kwargs
-        self.margin_scale = margin_scale
-        self.tail_size = tail_size
-
-
-    def fit(self, X, y=None):
-
-
-        self.gbm.fit(X, y)
-        X_emb = self.gbm.apply(X).reshape(X.shape[0], -1)
-        X_emb = self.bin.fit_transform(X_emb)
-        self.lr.fit(X_emb, y)
-
-    def predict(self, X, y=None, with_tree=False):
-        if with_tree:
-            preds = self.gbm.predict(X)
-        else:
-            X_emb = self.gbm.apply(X).reshape(X.shape[0], -1)
-            X_emb = self.bin.transform(X_emb)
-            preds = self.lr.predict(X_emb)
-        return preds
-
-    def predict_proba(self, X, y=None, with_tree=False):
-        if with_tree:
-            preds = self.gbm.predict_proba(X)
-        else:
-            X_emb = self.gbm.apply(X).reshape(X.shape[0], -1)
-            X_emb = self.bin.transform(X_emb)
-            preds = self.lr.predict_proba(X_emb)
-        return preds
-
+# ----------------------------------------- #
+# -- auxiliary functions
+# ----------------------------------------- #
 
 def fit_weibull(
-    observation
-    , distances
-    , margin_scale=1  # ?? For some reason this is used to scale the distances in distance matrix
+        observation
+        , distances
+        , return_distances=False
         ):
 
     """
@@ -68,6 +27,7 @@ def fit_weibull(
     :param observation: an array containing the observation features (vector)
     :param distances: distances between `observation` and observations from other classes.
     :param margin_scale: For some reason this is used to scale the distances in distance matrix.
+    :param return_distances: if `True` return the parameter `distances` in the results.
     :return: a weibull model for the extreme value of the observation
     """
     # observation = [-1.91979003066328, -0.803488581854428]
@@ -80,11 +40,17 @@ def fit_weibull(
     # ------------------------- #
     # -- fit weibull -- #
     # ------------------------- #
+
     params = stats.exponweib.fit(distances, floc=0, f0=1)
+    exp1, k1, loc1, lam1 = params
+    shape_ = k1
+    scale_ = lam1
 
     weibull_pars = dict(
-        shape=params[1]
-        , scale=params[3])
+        shape=shape_
+        , scale=scale_
+        , params = params
+    )
 
     # ------------------------- #
     # -- results
@@ -92,7 +58,275 @@ def fit_weibull(
     results = dict(
         weibull_pars=weibull_pars
         , observation=observation
-        , distances=distances
     )
+    if return_distances:
+        results['distances']=distances
+
+
+    return results
+
+
+
+def predict_weibul(
+        x
+        , weibull_pars
+        , lower_tail = True
+):
+    """
+    Make weibull predictions with given parameters
+    :param x: quantiles
+    :param weibull_pars: parameters for the weibull distribution. if lenth=2, then considers exp1=1, and loc1=0
+    :return:
+    """
+    if len(weibull_pars) == 2:
+        exp1, k1, loc1, lam1 = 1, weibull_pars[1], 0, weibull_pars[3]
+    else:
+        exp1, k1, loc1, lam1 = weibull_pars
+
+    result = stats.exponweib.cdf(x, exp1, k1, loc1, lam1)
+
+    if lower_tail:
+        result = 1-result
+
+    return result
+
+
+def predict_from_weibulls_dict_(
+        X
+        , weibulls_dict
+        , n_obs_to_fuse=4
+        , return_dict = True):
+    """
+    Predict form a `weibulls_dict`.
+    :param X: "test" observation to predict to. Must be a `(1,n_col)` array (n_col being the number of features)
+    :param weibulls_dict: `EVM.weibulls_dict` resulting from `EVM.fit()`
+    :param n_obs_to_fuse: number of observations to use in the prediction by averaging out their individual predictions
+    :param return_dict: if True, returns a dictionary. If false, return a list.
+    :return:
+    """
+    # X = X_pred
+    # X = X_pred[0, ]
+    # X = X_pred[4:5, ]
+    assert X.shape[0] == 1, 'X is not an array fo shape (1, nnumber of features)'
+
+    # ---------------------------------------- #
+    # -- get probability for each class
+    # ---------------------------------------- #
+    probs_by_class = {i: [] for i in weibulls_dict.keys()}  # to save the probabilities predicitons for each class
+    i_labels = -1
+    while i_labels < len(weibulls_dict) - 1:
+        i_labels = i_labels + 1
+        str_label = list(weibulls_dict.keys())[i_labels]
+
+        for weibull in weibulls_dict[str_label]:
+            # weibull = weibulls_dict[str_label][0]
+            distance = distance_matrix(weibull['observation'].reshape(1, -1), X.reshape(1, -1))
+
+            prob_prediction_ = predict_weibul(
+                x=distance
+                , weibull_pars=weibull['weibull_pars']['params']
+            )
+
+            assert len(prob_prediction_) == 1
+            prob_prediction_ = prob_prediction_[0][0]
+            probs_by_class[str_label].append(prob_prediction_)
+            del prob_prediction_
+
+
+    # --------------------------------------------------------------- #
+    # -- get top predictions to average out (n_obs_to_fuse) -- #
+    # --------------------------------------------------------------- #
+    for key_, value_ in probs_by_class.items():
+        value_.sort(reverse=True)
+        if n_obs_to_fuse is not None:
+            value_ = value_[:n_obs_to_fuse]
+        probs_by_class[key_] = value_
+
+    predictions = {key_: np.mean(value_) for key_, value_ in probs_by_class.items()}
+    if not return_dict:
+        predictions = list(predictions.values())
+
+    # ------------------ #
+    # -- results
+    # ------------------ #
+    return predictions
+
+
+# ----------------------------------------- #
+# ----------------------------------------- #
+
+
+class EVM(BaseEstimator, ClassifierMixin):
+    """Fits a logistic regression model on tree embeddings.
+    """
+    def __init__(self
+                 , margin_scale=1
+                 , tail_size=20
+                 , n_obs_to_fuse=4
+                 , confidence_threshold=0.9
+                 , **kwargs
+    ):
+        """
+
+        :param margin_scale: For some reason this is used to scale the distances in distance matrix.
+        :param tail_size: number of observations to use to fit the weibull distribution
+        :param n_obs_to_fuse: number of observations to use in the prediction by averaging out their individual predictions
+        :param confidence_threshold: Minimum probability of belonging to the acceptance area of the observation
+        :param kwargs:
+        """
+
+        self.kwargs = kwargs
+        self.margin_scale = margin_scale
+        self.tail_size = tail_size
+        self.confidence_threshold = confidence_threshold
+
+        if n_obs_to_fuse < 1:
+            n_obs_to_fuse = None
+        self.n_obs_to_fuse = n_obs_to_fuse
+
+
+    def fit(self, X, y=None):
+        """
+
+        :param X: Features
+        :param y: observed classes
+        :return:
+        """
+
+        tail_size = self.tail_size
+        margin_scale = self.margin_scale
+
+        # -------------------------- #
+        # -------------------------- #
+        self.labels = np.unique(y)
+
+        # -------------------------- #
+        # -- Get all distances
+        # -------------------------- #
+
+        dist_mat = distance_matrix(X, X)
+
+        # -------------------------- #
+        # -- Fit one weibull for each observation
+        # -------------------------- #
+        # weibulls = []
+        weibulls_dict = {i: [] for i in np.unique(y)}
+        i_observation = -1
+        while i_observation < y.shape[0]-1:
+            i_observation = i_observation + 1
+
+            # -- observation features and label -- #
+            obs_features = X[i_observation, ]
+            obs_label = y[i_observation]
+
+            # -- distances to other classes -- #
+            obs_distances = dist_mat[i_observation, y != obs_label]
+            obs_distances = np.sort(obs_distances)
+            if tail_size is not None:
+                # -- handle when there are too few observations for the tail size
+                if tail_size > len(obs_distances):
+                    obs_distances = obs_distances[:len(obs_distances)]
+                else:
+                    obs_distances = obs_distances[:tail_size]
+
+            obs_distances = obs_distances*margin_scale
+
+            # -- fit weibull -- #
+            obs_weibull = fit_weibull(
+                observation=obs_features
+                , distances=obs_distances)
+            obs_weibull['label'] = obs_label
+            weibulls_dict[obs_label].append(obs_weibull)
+
+        # -- Save results -- #
+        self.weibulls_dict = weibulls_dict
+
+
+    def predict_proba(
+            self
+            , X
+            , return_dict = True
+            , n_obs_to_fuse = None
+    ):
+        """
+        Predictions using the weibulls
+        :param X: "test" observations to predict to.
+        :param return_dict: if True, returns a dictionary. If false, return a list.
+        :param n_obs_to_fuse: number of observations to use in the prediction by averaging out their individual predictions
+        :param confidence_threshold: Minimum probability of belonging to the acceptance area of the observation
+        :return:
+        """
+
+        # -- get hyper parameters -- #
+        weibulls_dict = self.weibulls_dict
+
+        if n_obs_to_fuse is None:
+            n_obs_to_fuse = self.n_obs_to_fuse
+
+        # --------------------------- #
+        # -- make predicitons
+        # for each test observation
+        # --------------------------- #
+
+        predictions = []
+        i_obs = -1
+        while i_obs < X.shape[0]-1:
+            i_obs = i_obs + 1
+            X_aux = X[i_obs, ]
+            X_aux = X_aux.reshape(1, -1)
+
+            pred_ = predict_from_weibulls_dict_(
+                X_aux
+                , weibulls_dict=weibulls_dict
+                , return_dict = return_dict
+                , n_obs_to_fuse = n_obs_to_fuse
+            )
+
+            predictions.append(pred_)
+
+        if not return_dict:
+            # -- transform to array -- #
+            # predictions_array = [list(i.values()) for i in predictions]
+            predictions = np.array(predictions)
+
+
+        return predictions
+
+    def predict(
+            self
+            , X
+            , n_obs_to_fuse = None
+            , confidence_threshold = None):
+
+        labels = self.labels
+        if confidence_threshold is None:
+            confidence_threshold = self.confidence_threshold
+
+        # ------------------------------- #
+        # -- get probability predictions -- #
+        # ------------------------------- #
+        predictions = self.predict_proba(X
+            , return_dict=False
+            , n_obs_to_fuse=None
+            )
+
+        # ------------------------------- #
+        # -- predict label
+        # ------------------------------- #
+
+        proba_most_likely_label = np.apply_along_axis(lambda xx: np.max(xx), 1, predictions)
+        predicted_label_idx = np.apply_along_axis(lambda xx: np.where(xx == np.max(xx))[0], 1, predictions)
+        predicted_label_ = np.apply_along_axis(lambda xx: labels[xx], 0, predicted_label_idx)
+
+        # -- check if prediction of for unknown class (predicted value < confidence_threshold
+        predicted_label_[proba_most_likely_label < confidence_threshold] = -1
+        predicted_label_ = predicted_label_.reshape(-1, 1)
+
+        # ----------------------------- #
+        # -- result
+        # ----------------------------- #
+        return predicted_label_
+
+
 
 
